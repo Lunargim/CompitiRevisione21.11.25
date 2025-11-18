1a3a68c418afc23335db466d5c9f19d0193858c4
Lunargim <loryluna2001@gmail.com>
2025-11-18 12:23:00 +0100

    Fix: radius not changing with scale

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_EditorScripting/PlanetarySystem/Scripts/PlanetarySystem.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_EditorScripting/PlanetarySystem/Scripts/PlanetarySystem.cs
@@ -7,11 +7,14 @@ public class PlanetarySystem : MonoBehaviour
     {
         GameObject planet = Selection.gameObjects[0];
         var planetScale = planet.transform.localScale;
-        var radius = planet.GetComponent<SphereCollider>().radius;
+        //var radius = planet.GetComponent<SphereCollider>().radius;
+        var radius = planet.GetComponent<SphereCollider>().radius * Mathf.Max(planet.transform.lossyScale.x,
+            planet.transform.lossyScale.y, planet.transform.lossyScale.z);
         char[] digits = "0123456789".ToCharArray();
         var pos = planet.name.IndexOfAny(digits);
         var foo = planet.name.Substring(pos);
         var satellitesNumber = int.Parse(foo);
+        Debug.Log(radius);
 
         var maxDistance = 50;
         var minScale = 5;
@@ -19,7 +22,7 @@ public class PlanetarySystem : MonoBehaviour
 
         for (int i = 0; i < satellitesNumber; i++)
         {
-            var satellite = Instantiate(planet, planet.transform.position + new Vector3(Random.Range(radius*2, maxDistance), 0,0), Quaternion.identity);
+            var satellite = Instantiate(planet, planet.transform.position + new Vector3(Random.Range((radius + radius/2), maxDistance), 0,0), Quaternion.identity);
             var randomNumberScale = Random.Range(minScale, maxScale);
             satellite.transform.localScale = planetScale/randomNumberScale;
         }
