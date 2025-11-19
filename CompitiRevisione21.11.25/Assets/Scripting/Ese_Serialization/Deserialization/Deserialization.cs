609b6750ca056deb642a45f148939edcdf4798b5
Lunargim <loryluna2001@gmail.com>
2025-11-19 10:35:38 +0100

    Feat: add a script to serialize and deserialize

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Serialization/Deserialization/Deserialization.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Serialization/Deserialization/Deserialization.cs
@@ -1,16 +1,31 @@
+using System.IO;
 using UnityEngine;
 
 public class Deserialization : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public MyData data;
+    private void Awake()
     {
-        
+        Load();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Load()
     {
+        string jsonString = File.ReadAllText("C:/Users/loryl/Desktop/Esercizio Deserializzazione.txt");
+        JsonUtility.FromJsonOverwrite(jsonString, data);
         
+        //NEWTON 
+        // data = JsonConvert.DeserializeObject<MyData>(jsonString);
+    }
+    
+    [ContextMenu("Save File")]
+    public void Save()
+    {
+        string jsonString = JsonUtility.ToJson(data, true);
+        
+        //NEWTON
+        //string jsonString1 = JsonConvert.SerializeObject(data, Formatting.Indented);
+        
+        File.WriteAllText("C:/Users/loryl/Desktop/Esercizio Deserializzazione.txt", jsonString);
     }
 }
