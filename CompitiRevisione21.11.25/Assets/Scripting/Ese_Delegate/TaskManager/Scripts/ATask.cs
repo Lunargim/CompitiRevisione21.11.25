3daeffbb4053c843bd90296b714e50f01178ad59
Lunargim <loryluna2001@gmail.com>
2025-11-19 12:12:03 +0100

    modified: change the Atask abstract class

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/ATask.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/ATask.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 
-public abstract class ATask
+public abstract class ATask : MonoBehaviour
 {
-    private string _taskId;
-    private string _taskName;
-    private string _description;
-    private bool _isStarted;
-    private bool _isCompleted;
+    public abstract string taskId  { get; set; }
+    public abstract string taskName { get; set; }
+    public abstract string description { get; set; }
+    public abstract bool isStarted { get; set; }
+    public abstract bool isCompleted { get; set; }
 
     public delegate void OnStart();
     public event OnStart OnTaskStart;
     
     public delegate void OnComplete();
     public event OnComplete OnTaskCompleted;
-    
+
+    public abstract void Click();
+
 }
