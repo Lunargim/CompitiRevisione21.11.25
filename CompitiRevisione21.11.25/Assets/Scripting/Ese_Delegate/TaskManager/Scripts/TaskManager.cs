de1f188ad1225683ef686f6ff4ad4080065c7d18
Lunargim <loryluna2001@gmail.com>
2025-11-19 16:28:00 +0100

    Feat: start taskmanager (to be finished)

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/TaskManager.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/TaskManager.cs
@@ -1,30 +1,71 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskManager : MonoBehaviour
 {
     [SerializeField] public List<ATask> Tasks = new List<ATask>();
+    [SerializeField] public TextMeshProUGUI textTask;
+    [SerializeField] public Button startButton;
+    [SerializeField] public Button completedButton;
+    private int _index = 0;
+    public ATask currentTask;
+    public ATask nextTask;
+
+    public void Start()
+    {
+        currentTask = Tasks[_index];
+        EnableButtons();
+    }
     
+    public void DisplayText()
+    {
+        textTask.text = currentTask.description;
+    }
+    
+    public void Update()
+    {
+        if (currentTask.isStarted)
+        {
+            DisplayText();
+            currentTask.CheckClicks();
+        }
+    }
 
 
+    public ATask GetNextTask(int index)
+    {
+        nextTask = Tasks[index+1];
+        return nextTask;
+    }
 
 
+    public void DisableButtons()
+    {
+        startButton.enabled = false;   
+        completedButton.enabled = false;
+    }
 
+    public void EnableButtons()
+    {
+        startButton.enabled = true;
+        completedButton.enabled = true;
+    }
 
-
-
-
-
-
+    public void SetStartingTask()
+    {
+        currentTask.isStarted = true;
+    }
 
     public void OnEnable()
     {
-        
+        BottonScriptStart.OnBottonClicked += SetStartingTask;
     }
 
     public void OnDisable()
     {
-        
+        BottonScriptStart.OnBottonClicked -= SetStartingTask;
     }
     
 }
