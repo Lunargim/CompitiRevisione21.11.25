25cf7530a217d6b821f5506e25b6d2e20a2ce702
Lunargim <loryluna2001@gmail.com>
2025-11-19 15:15:34 +0100

    change: changed event to action

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/ATask.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Delegate/TaskManager/Scripts/ATask.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public abstract class ATask : MonoBehaviour
@@ -7,13 +8,11 @@ public abstract class ATask : MonoBehaviour
     public abstract string description { get; set; }
     public abstract bool isStarted { get; set; }
     public abstract bool isCompleted { get; set; }
-
-    public delegate void OnStart();
-    public event OnStart OnTaskStart;
     
-    public delegate void OnComplete();
-    public event OnComplete OnTaskCompleted;
+    public abstract event Action OnTaskStart;
+    
+    public abstract event Action OnTaskCompleted;
 
-    public abstract void Click();
+    public abstract void CheckClicks();
 
 }
