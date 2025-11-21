3832bca35edd392f9191c6f5f68f12ff147fb057
Lunargim <loryluna2001@gmail.com>
2025-11-21 09:58:12 +0100

    chore: fix minor errors

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Serialization/Deserialization/Deserialization.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Serialization/Deserialization/Deserialization.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 public class Deserialization : MonoBehaviour
 {
     public MyData data;
-    private void Start()
-    {
-        Load();
-    }
-
-    public void Load()
-    {
-        if(File.Exixts(jsonString))
-        {
-            string jsonString = File.ReadAllText("C:/Users/loryl/Desktop/Esercizio Deserializzazione.txt");
-            Mydata = JsonUtility.FromJsonOverwrite(jsonString, data);
-            
-            //JsonConvert.DeserializeObject<MyData>(jsonString);
-        }else
-        {
-            Debug.Log("File Json not found");
-        }
-        
-    }
+     private void Start()
+     {
+         Load();
+     }
+ 
+     public void Load()
+     {
+         string fileJson = Path.Combine("C:/Users/loryl/Desktop/Esercizio Deserializzazione.txt");
+         
+         if(File.Exists(fileJson))
+         {
+             string jsonString = File.ReadAllText(fileJson);
+             JsonUtility.FromJsonOverwrite(jsonString, data);
+             
+             //JsonConvert.DeserializeObject<MyData>(jsonString);
+         }else
+         {
+             Debug.Log("File Json not found");
+         }
+         
+     }
 }
