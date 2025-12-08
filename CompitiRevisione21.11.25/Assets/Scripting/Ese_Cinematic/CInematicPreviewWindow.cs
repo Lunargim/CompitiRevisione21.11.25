62ad1618aadfe1ac88ec8c625ca74cb2a941daea
Lunargim <loryluna2001@gmail.com>
2025-12-08 17:36:23 +0100

    import : lession 1/2

--- CompitiRevisione21.11.25/Assets/Scripting/Ese_Cinematic/CInematicPreviewWindow.cs
+++ CompitiRevisione21.11.25/Assets/Scripting/Ese_Cinematic/CInematicPreviewWindow.cs
@@ -7,25 +7,31 @@ public class CInematicPreviewWindow : EditorWindow
     private static CinematicCameraMovementData _movementData;
 
     [MenuItem("Tools/CinematicPreview")]
-    private void ShowWindow()
+    private static void ShowWindow()
     {
         GetWindow<CInematicPreviewWindow>();
+    }
 
-        if (_movementData == null)
-        {
-            var window = GetWindow<CInematicPreviewWindow>();
-            window.Close();
-        }
-
+    void OnEnable()
+    {
+        //_movementData = target as CinematicCameraMovementData;
     }
 
     public void OnGUI()
     {
         ShowWindow();
 
-        //qua vanno i metodi
+        InsertCinematicCameraScriptables();
+        
 
     }
 
+    private void InsertCinematicCameraScriptables()
+    {
+        GUILayout.Label("Inserisci lo scriptable");
+        GUILayout.Space(EditorGUIUtility.singleLineHeight);
+        EditorGUILayout.ObjectField("Scriptables", _movementData, typeof(CinematicCameraMovementData), false);
+        //var _data = Target as CinematicCameraMovementData;
+    }
 
 }
