b614326d296c2ddebecfcf36692ebd0e76865134
Lunargim <loryluna2001@gmail.com>
2025-12-18 18:23:49 +0100

    save for ending working day

--- CompitiRevisione21.11.25/Assets/Esercizi/Ese_MapEditorWindow/Editor/MapEditorWizard.cs
+++ CompitiRevisione21.11.25/Assets/Esercizi/Ese_MapEditorWindow/Editor/MapEditorWizard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -12,6 +13,9 @@ public class MapEditorWizard : EditorWindow
     private Dictionary<string, GameObject> _prefabDictionary = new Dictionary<string, GameObject>();
     private string _alphabet = "abcdefghijklmnopqrstuvwxyz";
     private Vector2 _prefabScrollPosition;
+    private List<int> _buttonsPressedIndex = new List<int>();
+    private List<string> _buttonsLetter = new List<string>();
+    
 
     [MenuItem("Tools/Map Editor")]
 
@@ -23,26 +27,32 @@ public class MapEditorWizard : EditorWindow
     
     private void OnGUI()
     {
+        LoadButtonsLetter();
         DrawSizeMap();
+        EditorGUILayout.Space(30);
         DrawPrefabList();
+        EditorGUILayout.Space(30);
+        DrawMap();
     }
 
     private void OnFocus()
     {
-        _prefab = Resources.LoadAll<GameObject>( "Assets/Esercizi/Ese_MapEditorWindow/Prefab").ToList();
+        _prefab = Resources.LoadAll<GameObject>( "ResourceEseEditorMap/Prefab").ToList();
     }
  
 
     private void DrawSizeMap()
     {
-        _mapSize = EditorGUILayout.Vector2IntField("Map size", _mapSize);
+        _mapSize = EditorGUILayout.Vector2IntField("Map size", _mapSize, GUILayout.Width(200));
     }
 
     private void DrawPrefabList()
     {
         LoadDictionary();
+        
         GUILayout.BeginVertical();
-        _prefabScrollPosition = EditorGUILayout.BeginScrollView(_prefabScrollPosition, false, true);
+        
+        _prefabScrollPosition = EditorGUILayout.BeginScrollView(_prefabScrollPosition, false, true,  GUILayout.Height(150), GUILayout.Width(150));
 
         DrawPrefabDicitonary();
         
@@ -68,13 +78,79 @@ public class MapEditorWizard : EditorWindow
         GUILayout.BeginVertical();
         foreach (var item in _prefabDictionary)
         {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label(item.Key);
-            GUILayout.Space(20);
-            GUILayout.Label(item.Value.name);
+            GUILayout.BeginHorizontal(GUILayout.Width(20));
+            if(GUILayout.Button(item.Key, GUILayout.Width(30)))
+            {
+                GiveLetterToButton(item.Key);
+            }
+            GUILayout.Space(10);
+            GUILayout.Label(item.Value.name, GUILayout.Width(50));
             GUILayout.EndHorizontal();
         }
         GUILayout.EndVertical();
     }
     
+    private void DrawMap()
+    {
+        var prevBackground = GUI.backgroundColor;
+
+        /*foreach (var item in _buttonsPressed)
+        {
+            if (item)
+            {
+                GUI.backgroundColor = Color.yellow;
+            }
+        }*/
+        
+        GUILayout.BeginVertical();
+        for (int y = 0; y < _mapSize.y; y++)
+        {
+            GUILayout.BeginHorizontal();
+            for (int x = 0; x < _mapSize.x; x++)
+            {
+                if ( GUILayout.Button(_buttonsLetter[x], GUILayout.Width(30), GUILayout.Height(30)))
+                {
+                    Debug.Log("AAAAA");
+                    _buttonsPressedIndex.Add(x);
+                    Debug.Log(_buttonsPressedIndex[0]);
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+        GUI.backgroundColor = prevBackground;
+    }
+
+    private void GiveLetterToButton(string letter)
+    {
+        if (_buttonsPressedIndex.Count >= 1)
+        {
+            for (int i = 0; i < (_mapSize.x * _mapSize.y) ; i++)
+            {
+                if (_buttonsPressedIndex.Contains(i))
+                {
+                    _buttonsLetter.Add(letter);
+                }
+                else
+                {
+                    _buttonsLetter.Add("");
+                }
+            }
+        }
+        
+    }
+
+    private void LoadButtonsLetter()
+    {
+        if (_buttonsPressedIndex.Count < 1)
+        {
+            for (int i = 0; i < (_mapSize.x * _mapSize.y); i++)
+            {
+                _buttonsLetter.Add("");
+            }
+        }
+    }
+
 }
+
+
